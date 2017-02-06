61b78d0fa2d3b8f80aac0a485035e740b2155672
Jonathan Robic <hello@jonathanrobic.fr>
2017-02-06 14:18:50 +0100

    add Header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
+import { Header } from './components/common';
+
 
 const store = configureStore();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <View />
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
     </Provider>
   );
 };
